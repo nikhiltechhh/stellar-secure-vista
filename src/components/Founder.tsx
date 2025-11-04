18c59c7aac5cdf905b0739d3251e919ad90f4b50
Nikhil <nikhil@Ravis-MacBook-Air.local>
2025-11-04 20:41:20 +0530

    updateff

--- src/components/Founder.tsx
+++ src/components/Founder.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Award } from "lucide-react";
 import { Link } from "react-router-dom";
-import founder from "@/assets/founder.JPG";
+import founder from "@/assets/Naveen.jpg";
 import Vamsi from "@/assets/Vamsi.jpeg";
 
 const Founder = () => {
--- src/pages/Founderdetails.tsx
+++ src/pages/Founderdetails.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Award, ArrowLeft, CheckCircle, Target, Heart, Sparkles } from "lucide-react";
 import { Link } from "react-router-dom";
-import founderImg from "@/assets/founder.JPG";
+import founderImg from "@/assets/Naveen.jpg";
 
 const FounderDetail = () => {
   return (
